9754af6b34317d1ef2b1bcbed50e31975f995dec
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-05-19 11:05:07 -0400

    commit tree dd34f5462ea814bd2a26b54032a5f810d1c2a995

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, alive) VALUES("Mr. Chocolate", 2, "M", 1);
+INSERT INTO bears(name, age, sex, alive) VALUES("Rowdy", 5, "M", 1);
+INSERT INTO bears(name, age, sex, alive) VALUES("Tabitha", 10, "F", 1);
+INSERT INTO bears(name, age, sex, alive) VALUES("Sergeant Brown", 6, "M", 0);
+INSERT INTO bears(name, age, sex, alive) VALUES("Melissa", 6, "F", 1);
+INSERT INTO bears(name, age, sex, alive) VALUES("Grinch", 3, "M", 0);
+INSERT INTO bears(name, age, sex, alive) VALUES("Wendy", 7, "F", 1);
+INSERT INTO bears(name, age, sex, alive) VALUES(NULL, 4, "F", 1);
